3c7fc4cd87eb1f0b28a2f4d593430dacf03f8543
sacua <59889660+sacua@users.noreply.github.com>
2022-05-25 21:05:22 -0400

    Update Animal_Feeder.groovy

--- Animal_Feeder.groovy
+++ Animal_Feeder.groovy
@@ -6,10 +6,14 @@ metadata {
         attribute "foodGiven", "string"
         
         command "GiveFood", ["number"]
+        command "servo", ["number"]
+        command "setZero"
+        command "calibrate", ["number"]
+        
         
     }
     preferences {
-        input name: "IPAddress", type: "string", title: "IP Address of the ESP device", required: true, defaultValue: "192.168.0.1"
+        input name: "IPAddress", type: "string", title: "IP Address of the ESP device", required: true, defaultValue: "192.168.0.157"
     }
 }
 
@@ -20,7 +24,10 @@ def parse(String description) {
 
 def GiveFood(foodWeight) {
     try {
-        def params = [uri: "http://"+ IPAddress + "/data/?food_weight=" + foodWeight]
+        weightfood = foodWeight as float
+        String foodweight = (weightfood * state.calibrationFactor).toString();
+        
+        def params = [uri: "http://"+ IPAddress + "/data/?food_weight=" + foodweight]
         
         httpGet(params) { resp ->
             if (resp.success) {
@@ -30,7 +37,60 @@ def GiveFood(foodWeight) {
         runIn(65,"FoodGiven")
     }
     catch (Exception e1) {
+        sendEvent(name: "response", value: "Failed");
+        log.error "EC - Call failed: ${e1.message}"
+    }
+}
+
+def servo(servoPos) {
+    try {
+        def params = [uri: "http://"+ IPAddress + "/data/?servo=" + servoPos]
+        
+        httpGet(params) { resp ->
+            if (resp.success) {
+                sendEvent(name: "response", value: "Success");
+            }
+        }
+    }
+    catch (Exception e1) {
       sendEvent(name: "response", value: "Failed");
+      log.error "EC - Call failed: ${e1.message}"
+    }
+}
+
+def setZero() {
+    try {
+        def params = [uri: "http://"+ IPAddress + "/data/?readweight=0"]
+        
+        httpGet(params) { resp ->
+            if (resp.success) {
+                String currentValue = resp.data;
+                state.zero = Integer.parseInt(currentValue);
+            }
+        }
+    }
+    catch (Exception e1) {
+      sendEvent(name: "response", value: "Failed");
+      log.error "EC - Call failed: ${e1.message}"
+    }
+}
+
+def calibrate(weight) {
+    try {
+        def params = [uri: "http://"+ IPAddress + "/data/?readweight=0"]
+        
+        httpGet(params) { resp ->
+            if (resp.success) {
+                String currentValue = resp.data;
+                int CurrentValue = Integer.parseInt(currentValue);
+                weightCal = weight as float;
+                state.calibrationFactor = (CurrentValue - state.zero)/weightCal as float;
+            }
+        }
+    }
+    catch (Exception e1) {
+      sendEvent(name: "response", value: "Failed");
+      log.error "EC - Call failed: ${e1.message}"
     }
 }
 
@@ -46,5 +106,6 @@ def FoodGiven() {
     }
     catch (Exception e1) {
       sendEvent(name: "response", value: "Failed");
+      log.error "EC - Call failed: ${e1.message}"
     } 
 }
